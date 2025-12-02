f04385241e7a1bbba9bb68a1a250bbfb2fdf680e
Caio S. Duarte <caiosduarte@protonmail.com>
2025-12-02 19:46:56 +0000

    atualizando pelo console

--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, "agenda/post_list.html", {})
+    posts = Post.objects.order_by("-created_at")
+    return render(request, "agenda/post_list.html", {'post_list': posts})
